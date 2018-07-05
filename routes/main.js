3584ccad9f6e9266a5f15cb3baf43dab924666e9
chungsunhyeon <chungsun.hyeon@gentlepie.com>
2018-07-05 12:47:22 +0900

    update main.js

--- routes/main.js
+++ routes/main.js
@@ -8,11 +8,7 @@ var router = express.Router();
 // });
 // define the home page route
 router.get('/', function(req, res) {
-  res.send('Birds home page');
-});
-// define the about route
-router.get('/about', function(req, res) {
-  res.send('About birds');
+  res.send('http main home');
 });
 
 module.exports = router;
